9adc64f2be21e91652bafe7ff8e5055d9864b81d
wangangi <wangangi@iu.edu>
2022-02-22 19:09:00 -0500

    update

--- serve_testapp.bat
+++ serve_testapp.bat
@@ -1,4 +1,4 @@
-cd C:\Users\vcm\Desktop\testapp
-git pull https://%TESTAPP_GIT_PAT%@github.com/wangangi/testapp.git
+cd C:\Users\vcm\C:\testapp
+git pull https://%TESTAPP_GIT_PAT%@github.com/wangangi/homework_3-1.git
 venv\Scripts\python.exe -m pip install -r requirements.txt
 venv\Scripts\python.exe server.py
--- server.py
+++ server.py
@@ -2,4 +2,4 @@
 from waitress import serve
 import app
 
-serve(app.server, host='localhost', port=3000)
+serve(app.server, host='localhost', port=3001)
